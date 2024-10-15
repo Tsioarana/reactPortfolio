bd71acf8bc914acaa245269a1264f4993d681794
Angelot <angelottsiferana@gmail.com>
2024-10-15 14:48:19 +0300

    napina

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        // Séparation des modules en chunks plus petits
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            // Crée un chunk séparé pour chaque dépendance dans node_modules
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+      },
+    },
+    // Augmente la limite d'avertissement de taille des chunks à 2000 kB (ou plus selon vos besoins)
+    chunkSizeWarningLimit: 2000,
+  },
 })
